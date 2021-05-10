bdf85ccca64ca223d6d6cef278369e98a1e03514
vishchopade <vishal781992@gmail.com>
2021-05-09 20:13:39 -0600

    class_demo

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-def main(a, b, c):
-    # take the list and convert that list into a args print every arg
-    print("First name: %s, Last name: %s, gender: %s " % (a, b, c))
+class Car:
+    color = ""
+    make = ""
+
+    def __init__(self, color, make):
+        self.color = color
+        self.make = make
+
+    def inWarranty(self, year):
+        if year < 2020:
+            print("Yes, covers the warranty!")
+        else:
+            print("out of warranty!")
+
+    def isRepaired(self):
+        if "honda" in self.make:
+            print("Yes, the car is repaired!")
+
+        else:
+            print("Waiting for the response.")
+
+        # self.inWarranty(2021)
 
 
-if __name__ == '__main__':
-    list_x = ["vish", "chopade", "male"]
-    main(*list_x)
+def main():
+    car = Car("pink", "honda")
+    car.isRepaired()
+    car.inWarranty(2011)
+
+
+if __name__ == "__main__":
+    main()
